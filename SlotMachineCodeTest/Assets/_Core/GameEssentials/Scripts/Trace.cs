40ad1372acedb0a43a890a70fbc5fa2d034634da
Juan Vazquez <diclonius32@gmail.com>
2024-04-10 18:03:42 -0600

    * [Plugins] Game Essentials Update.

---
+++ SlotMachineCodeTest/Assets/_Core/GameEssentials/Scripts/Trace.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+public class Trace : MonoBehaviour {
+
+	[Conditional("USE_LOGS")]
+    public static void Log(string format, params object[] arguments)
+    {
+        UnityEngine.Debug.Log(string.Format(format, arguments));
+    }
+
+    [Conditional("USE_LOGS")]
+    public static void LogError(string format, params object[] arguments)
+    {
+        UnityEngine.Debug.LogError(string.Format(format, arguments));
+    }
+}
---
+++ SlotMachineCodeTest/Assets/_Core/GameEssentials/Scripts/UI_Framework/Core/ConfigData/UIAnimation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Threading.Tasks;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+
+namespace HannieEcho.UI.Data
+{
+    public abstract class UIAnimation : ScriptableObject
+    {
+        public abstract UniTask Animate(UIView view);
+        public abstract void Init();
+        public abstract UniTask AfterScreenInit();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="trans"></param>
+        /// <param name="offsetX"></param>
+        /// <param name="offsetY"></param>
+        /// <param name="animTime"></param>
+        /// <param name="ease"></param>
+        /// <returns></returns>
+        public Task SavePositionAndMove(Transform trans, float offsetX, float offsetY, float animTime, Ease ease = Ease.OutCirc)
+        {
+            Vector2 originPos = trans.localPosition;
+            SendOffScreen(trans, offsetX, offsetY);
+            return trans.DOLocalMove(originPos, animTime).SetEase(ease).AsyncWaitForCompletion();
+        }
+
+        public Vector2 SendOffScreen(Transform obj, float xOffset, float yOffset)
+        {
+            Vector2 originalPos = obj.localPosition;
+            obj.localPosition = new Vector2(obj.localPosition.x + xOffset, obj.localPosition.y + yOffset);
+            return originalPos;
+        }
+    }
+}
+
+public enum EAnimationDirection
+{
+    In,
+    Out
+}
